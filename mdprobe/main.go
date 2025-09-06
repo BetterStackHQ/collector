81d222a136f5e37e8ce04d719c8e8909027748a4
Paweł Wal <pawel@betterstack.com>
2025-09-06 16:19:44 +0200

    modify mdprobe to account for Azure zones

--- mdprobe/main.go
+++ mdprobe/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"regexp"
 
 	"github.com/coroot/coroot-node-agent/node/metadata"
 )
@@ -13,6 +14,12 @@ func main() {
 		fmt.Println("{}")
 		return
 	}
+	
+	// Modify AvailabilityZone if it's a decimal numeric string (Azure case)
+	if matched, _ := regexp.MatchString("^[0-9]+$", md.AvailabilityZone); matched {
+		md.AvailabilityZone = md.Region + "-" + md.AvailabilityZone
+	}
+	
 	out, _ := json.Marshal(md)
 	fmt.Println(string(out))
 }
